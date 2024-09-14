791474798af6e83addb5eb01e0aef509ba95261d
SystemCallBeam <tkhaeamadan@gmail.com>
2024-09-14 12:02:43 +0700

    adjust calculate

--- Cow.java
+++ Cow.java
@@ -1,12 +1,12 @@
 public class Cow {
     private String id;
-    //how to create enum data type?
+    // how to create enum data type?
     private String color;
     private int ageYears;
     private int ageMonths;
 
-    //how to recieve enum parametor?
-    Cow (String id, String color, int ageYears, int ageMonths) {
+    // how to recieve enum parametor?
+    Cow(String id, String color, int ageYears, int ageMonths) {
         this.id = id;
         this.color = color;
         this.ageYears = ageYears;
@@ -45,20 +45,46 @@ public class Cow {
         this.ageMonths = ageMonths;
     }
 
+    public String getMilk() {
+        switch (color) {
+            case "white":
+                return "plain";
+            case "brown":
+                return "chocolate";
+            case "pink":
+                return "strawberry";
+
+            default:
+                return "mystery";
+        }
+    }
+
     public int getTotalAgeInMonths() {
         return ageYears * 12 + ageMonths;
     }
 
-    public int calculateMilkProduction() {
+    public int calculateMilk() {
         switch (color) {
             case "white":
-                return 120 - getTotalAgeInMonths(); // Plain milk
+                return plainMilk(); // Plain milk
             case "brown":
-                return 40 - ageYears;               // Chocolate milk
+                return chocolateMilk(); // Chocolate milk
             case "pink":
-                return 30 - ageMonths;              // Strawberry milk
+                return strawberryMilk(); // Strawberry milk
             default:
                 return 0;
         }
+    }
+
+    public int plainMilk() {
+        return 120 - getTotalAgeInMonths();
+    }
+
+    public int chocolateMilk() {
+        return 40 - ageYears;
+    }
+
+    public int strawberryMilk() {
+        return 30 - ageMonths;
     }
 }
