0743e9b8d45b92e0883e019af3e160571b2f8fae
SystemCallBeam <tkhaeamadan@gmail.com>
2024-09-14 10:05:33 +0700

    add cow data type

---
+++ Cow.java
@@ -0,0 +1,16 @@
+public class Cow {
+    private int code;
+    //how to create enum data type?
+    private Color color;
+    private int age_years;
+    private int age_months;
+
+    //how to recieve enum parametor?
+    Cow (int code, Color color, int age_years, int age_months) {
+        this.code = code;
+        this.color = color;
+        this.age_years = age_years;
+        this.age_months = age_months;
+    }
+    
+}
